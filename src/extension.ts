e38539376e7b5afab3d903f1a3053de1bfe70c51
Akshit8 <akshit@dyte.io>
2023-08-18 01:35:36 +0530

    fix: code to open a new folder

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,6 @@ import * as vscode from 'vscode';
 import { LinearIssue, LinearIssueProvider } from './ui/TreeDataProvider';
 import { getWebviewContent } from './ui/TicketDetailWebView';
 import { Issue } from '@linear/sdk';
-import Hyperbeam from '@hyperbeam/web';
 
 const linearAPIStorageKey = 'LINEAR_API_STORAGE_KEY';
 
@@ -57,7 +56,7 @@ export function activate(context: vscode.ExtensionContext) {
 			message => {
 			  switch (message.command) {
 				case 'openineditor':
-				  vscode.commands.executeCommand('ticket-connect.openTicketInEditor', [issue]);
+				  vscode.commands.executeCommand('ticket-connect.openTicketInEditor', [issue.branchName]);
 				  return;
 			  }
 			},
@@ -66,31 +65,62 @@ export function activate(context: vscode.ExtensionContext) {
 		);
 	});
 
-	vscode.commands.registerCommand('ticket-connect.openTicketInEditor', async () => {
-		exec('cd /Users/akshit/Desktop/test-repo && git checkout -b test-branch-3');
-		exec('cd /Users/akshit/Desktop/test-repo && git commit -m "test commit" --allow-empty', (err, stdout, stderr) => {
-			if (err) {
-				return;
-			}
-			exec('cd /Users/akshit/Desktop/test-repo && git push origin test-branch-3');
-		});
+	vscode.commands.registerCommand('ticket-connect.openTicketInEditor', async (branchName: string) => {
+		console.log('got', branchName);
+		// const branchName = issue.branchName;
 
-		exec('ls ../', (err, stdout, stderr) => {
+		const baseFolder = '/Users/akshit/dyte';
+
+		exec(`ls ${baseFolder}`, (err, stdout, stderr) => {
 			if (err) {
+				console.log('err', err);
 				return;
 			}
 
 			const folders = stdout.split('\n').filter((x) => x);
 
 			const quickPick = vscode.window.createQuickPick();
-			quickPick.items = folders.map((folder) => ({ label: `/Users/akshit/dyte/${folder}` }));
+			quickPick.items = folders.map((folder) => ({ label: `${baseFolder}/${folder}` }));
 			
+			quickPick.onDidChangeSelection((selection) => {
+				const folderPath = selection[0].label;
+
+				exec(`cd ${folderPath} && git checkout -b ${branchName}`, (err, stdout, stderr) => {
+					if (err) {
+						console.log('err', err);
+						return;
+					}
+				});
+
+				exec(`cd ${folderPath} && git commit -m "branch created from ticket-connect" --allow-empty`, (err, stdout, stderr) => {
+					if (err) {
+						console.log('err', err);
+						return;
+					}
+
+					console.log('stdout', stdout);
+
+					exec('pwd', (err, stdout, stderr) => {
+						console.log('pwd', stdout);
+					});
+
+					exec(`cd ${folderPath} && git push origin ${branchName}`, (err, stdout, stderr) => {
+						if (err) {
+							console.log('err', err);
+							return;
+						}
+					});
+				});
+				
+				
+				const uri = vscode.Uri.file(folderPath);
+				vscode.commands.executeCommand('vscode.openFolder', uri);
+				quickPick.hide();
+			});
+
 			quickPick.onDidHide(() => quickPick.dispose());
 			quickPick.show();
 		});
-		
-		// let uri = vscode.Uri.file('');
-		// let success = await vscode.commands.executeCommand('vscode.openFolder', uri);
 	});
 	
 }
